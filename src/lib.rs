0b63d7215226660f3b2dc44f64cb5c7025bf8d70
Grzegorz Blach <grzegorz@blach.pl>
2023-11-19 17:02:36 +0100

    Add copyright note to source files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use base64::{Engine, engine::general_purpose};
 use chacha20poly1305::XChaCha20Poly1305;
 use chacha20poly1305::aead::{Aead, KeyInit, Payload};
